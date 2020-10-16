31167b410b50815e373facd43998dba615109bfa
switch-coder <sang_hk@naver.com>
2020-10-16 20:47:41 +0900

    Updates

---
+++ precache-manifest.cb5c8f9c027fa133f84d7803cf4e2a86.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "923139422a7eeb5b4815bbcb80eb4956",
+    "url": "/card-wallet/index.html"
+  },
+  {
+    "revision": "6bdac74a59e3efc14e60",
+    "url": "/card-wallet/static/css/main.08eef017.chunk.css"
+  },
+  {
+    "revision": "1cd65df26bbe3f5cb5c3",
+    "url": "/card-wallet/static/js/2.9e01b049.chunk.js"
+  },
+  {
+    "revision": "da42135372d3365a7233548ad1382c4d",
+    "url": "/card-wallet/static/js/2.9e01b049.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bdac74a59e3efc14e60",
+    "url": "/card-wallet/static/js/main.bdc6a524.chunk.js"
+  },
+  {
+    "revision": "74b199eeceb2fddfe3a1",
+    "url": "/card-wallet/static/js/runtime-main.73eae6e2.js"
+  },
+  {
+    "revision": "8a353cbcfd056f70898db63fda3fb108",
+    "url": "/card-wallet/static/media/103.8a353cbc.png"
+  },
+  {
+    "revision": "6d70709b511f6b958b0516464d8319f4",
+    "url": "/card-wallet/static/media/109.6d70709b.png"
+  },
+  {
+    "revision": "40affce75acf2fa9acf1b1007eaf11d5",
+    "url": "/card-wallet/static/media/114.40affce7.png"
+  },
+  {
+    "revision": "1e9f9e799db2bdb2a0a2cb0c905be18e",
+    "url": "/card-wallet/static/media/122.1e9f9e79.png"
+  },
+  {
+    "revision": "d9924cbe88b0cfaf2e3a5be52dc17581",
+    "url": "/card-wallet/static/media/126.d9924cbe.png"
+  },
+  {
+    "revision": "05123ef9c70c9f3cc2e3652c1b6b06b0",
+    "url": "/card-wallet/static/media/129.05123ef9.png"
+  },
+  {
+    "revision": "eb9e66a98b9f5b98dcdc4f26077ebc8e",
+    "url": "/card-wallet/static/media/15.eb9e66a9.png"
+  },
+  {
+    "revision": "be628bbdc27bd307688cd1f2659a526d",
+    "url": "/card-wallet/static/media/17.be628bbd.png"
+  },
+  {
+    "revision": "af745c6f8fe0b68b23d94d7c1f951d8a",
+    "url": "/card-wallet/static/media/23.af745c6f.png"
+  },
+  {
+    "revision": "fba40c398dd1716faaddb9a93b489f00",
+    "url": "/card-wallet/static/media/30.fba40c39.png"
+  },
+  {
+    "revision": "07ff930bdeb64ce04c6d537d9e8f2fdd",
+    "url": "/card-wallet/static/media/48.07ff930b.png"
+  },
+  {
+    "revision": "10726f67495bef4c828e64a1bcd9075a",
+    "url": "/card-wallet/static/media/53.10726f67.png"
+  },
+  {
+    "revision": "5d6e31c33347e9f7705e3000ae9cda6b",
+    "url": "/card-wallet/static/media/58.5d6e31c3.png"
+  },
+  {
+    "revision": "f35caa73a19954ba76b15b68c2d128e4",
+    "url": "/card-wallet/static/media/61.f35caa73.png"
+  },
+  {
+    "revision": "3678e287925763e796843ef108a0240c",
+    "url": "/card-wallet/static/media/86.3678e287.png"
+  },
+  {
+    "revision": "fafe7d19db2f6a62dab67ad3957995c2",
+    "url": "/card-wallet/static/media/98.fafe7d19.png"
+  },
+  {
+    "revision": "12ebc636d55d96a90264b96d97bf6087",
+    "url": "/card-wallet/static/media/99.12ebc636.png"
+  }
+]);
